c0e8ab167c1953ee4ea24d5d779244c8bfeadd62
Wilmer Cárdenas <wacardenas533@soy.sena.edu.co>
2023-04-24 20:02:34 -0500

    Se organiza para que quede con la lógica del CRUD

--- js/controllers/cecoController.js
+++ js/controllers/cecoController.js
@@ -1,32 +1,22 @@
+import { cecoService } from '../services/cecoService.js';
 
-import {cecoService} from '../services/cecoService.js'
-import {DataTable} from '../datatable.js'
-
-import {open_close_menu} from '../dropdownMenu.js'
-import {list} from '../main.js';
+const ceco = new cecoService();
 
 async function listcostcenter() {
-    const ceco = new cecoService();
-    const cecos = await ceco.obtenerRegistros();
-
-    const dt = new DataTable()
-    dt.createtable(cecos);
-    open_close_menu(list);
+    return ceco.obtenerRegistros();
 }
 
 async function addCeco(addData) {
-    const ceco = new cecoService();
-    const rs = await ceco.crearRegistro(addData);
+    return ceco.crearRegistro(addData);
 }
 
 async function editCeco(id, editData) {
-    const ceco = new cecoService();
-    const rs = await ceco.actualizarRegistro(id, editData);
+    return ceco.actualizarRegistro(id, editData);
 }
 
 async function delCeco(delData) {
-    const ceco = new cecoService();
     const rs = await ceco.eliminarRegistro(delData);
+    console.log(rs);
 }
 
 export { listcostcenter, addCeco, editCeco, delCeco };
